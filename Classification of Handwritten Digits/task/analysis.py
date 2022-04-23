6f88adb59cda08e595c496114ae9477315a27409
Eyal_Bahar <eyal.bhr@gmail.com>
2022-04-23 23:28:08 +0700

    stage 2 omplete

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -9,8 +9,8 @@ def set_shape(data: np.array, shape: tuple = (60000, 784)) -> np.array:
 
 def data_loader():
     # Load data for test and train
-    tf.keras.datasets.mnist.load_data(path="mnist.npz")
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+
     """
     tf.keras.datasets.mnist.load_data(path="mnist.npz") - >  
     assert x_train.shape == (60000, 28, 28)
@@ -19,11 +19,9 @@ def data_loader():
     assert y_test.shape == (10000,)
     """
 
-
-
     train_n = 60000
     train_m = 28 * 28
-    x_train = set_shape(x_train, (train_n,train_m))
+    x_train = set_shape(x_train, (train_n, train_m))
 
     # reshpae:
 
@@ -31,11 +29,26 @@ def data_loader():
     train_min = x_train.min()
 
     # print data_loader summary
-    print(f"Classes: {np.unique(y_test)}")
-    print(f"Features' shape: ({n}, {m})")
-    print(f"Target's shape: ({n},)")
-    print(f"min: {train_min}", f"max: {train_max}")
+    # print(f"Classes: {np.unique(y_test)}")
+    # print(f"Features' shape: ({n}, {m})")
+    # print(f"Target's shape: ({n},)")
+    # print(f"min: {train_min}", f"max: {train_max}")
+    return x_train, y_train
 
+def train_test_hanlder(features, target):
+    from sklearn.model_selection import train_test_split
+    import pandas as pd
+    x_train, x_test, y_train, y_test = train_test_split(features, target, random_state=40, test_size=0.3)
+    print(f"x_train shape:", x_train.shape)
+    print(f"x_test shape:", x_test.shape)
+    print(f"y_train shape:", y_train.shape)
+    print(f"y_test shape:", y_test.shape)
+    print("Proportion of samples per class in train set:")
+    # print(x_train.value_counts(normalize=True))
+    print(pd.DataFrame(y_train).value_counts(normalize=True))
 
 if __name__ == '__main__':
-    data_loader()
+    x_train, y_train = data_loader()
+    x_train = x_train[0:6000, :]
+    y_train = y_train[0:6000]
+    train_test_hanlder(x_train, y_train)
