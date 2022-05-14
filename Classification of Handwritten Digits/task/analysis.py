80aff23f0277c57e471863ea0c1617ed29c0e23f
Eyal_Bahar <eyal.bhr@gmail.com>
2022-05-14 13:09:35 +0200

    project complete

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -10,8 +10,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-
 import operator
+from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import GridSearchCV
 
 class Loader:
     """ load raw data and reshape"""
@@ -103,15 +104,42 @@ def fit_predict_eval(runner: BaseRunner):
     runner.set_score()
     runner.print_results()
 
-
-def print_best(scores):
+def find_best_score(scores):
     key = max(scores.items(), key=operator.itemgetter(1))[0]
-    model_name = f"{key}".split("(")[0] + "()"
-    print(f"The answer to the question: {model_name} - {round(scores[key],3)}")
+    return key, scores[key]
+
+
+def format_name_and_score(best_model_name, best_score):
+    model_name = f"{best_model_name}".split("(")[0] + "()"
+    round_score = round(best_score, 3)
+    return model_name, round_score
+
+
+def remove_sister_models(scores, name1):
+    for k in list(scores.keys()):
+        if k.startswith(name1[:-1]):
+            del scores[k]
+    return scores
+
+
+def print_best(scores, n=1):
+    best_model_name, best_score = find_best_score(scores)
+    name1, val1 = format_name_and_score(best_model_name, best_score)
+    if n == 1:
+
+        print(f"The answer to the question: {name1} - {val1}")
+        return
+    scores = remove_sister_models(scores, name1)
+    if n == 2:
+        second_best_name, second_best_score = find_best_score(scores)
+        name2, val2 = format_name_and_score(second_best_name, second_best_score)
+        print(f"The answer to the 2nd question: {name1}-{val1}, {name2}-{val2} ")
 
 
 
-def stage_3(train_feats, train_target,  test_feats, test_target):
+
+
+def run_evaluate_models(train_feats, train_target,  test_feats, test_target, print_in_run=0):
     RANDOM_STATE = 40
     models = [KNeighborsClassifier(),
               DecisionTreeClassifier(random_state=RANDOM_STATE),
@@ -128,10 +156,36 @@ def stage_3(train_feats, train_target,  test_feats, test_target):
                             test_target)
         runner.train()
         runner.set_score()
-        runner.print_results()
-
+        if print_in_run:
+            runner.print_results()
         scores.update({f"{model}": runner.score})
-    print_best(scores)
+    return scores
+
+
+def compare_score_set(scores, scores_from_normed):
+    best_model, best_score = find_best_score(scores)
+    best_model_normed, normed_best_score = find_best_score(scores_from_normed)
+    return normed_best_score > best_score
+
+
+def print_if_norm_helped(scores, scores_from_normed):
+    is_b_better = compare_score_set(scores, scores_from_normed)
+    if is_b_better:
+       helped = "yes"
+    else:
+        helped = "no"
+    print(f"The answer to the 1st question: {helped}\n")
+
+
+def mark_scores_names_as_normed(scores_from_normed):
+    refactored_score_names = {f"{key}_normed": value for key, value in scores_from_normed.items()}
+    return refactored_score_names
+
+
+def check_if_normed_helped_report(scores, scores_from_normed):
+    print_if_norm_helped(scores, scores_from_normed)
+    print_best(scores | scores_from_normed, 2)
+
 
 if __name__ == '__main__':
     ##
@@ -141,9 +195,62 @@ if __name__ == '__main__':
 
     loader.x_train = loader.set_shape(loader.x_train)
     x_train, y_train = loader.small_sample()
-    ##
+    ## stage two
     x_train, x_test, y_train, y_test = Splitter.train_test_hanlder(x_train, y_train)
     # Splitter.stage_two_report(x_train, x_test, y_train, y_test)
-    stage_3(x_train, y_train, x_test, y_test)
+    ## Norm results
+    transformer = Normalizer().fit(x_train)  # fit does nothing.
+    normed_x_train = transformer.transform(x_train)
+    normed_x_test = transformer.transform(x_test)
+
+    ## Stage 3 Run through models
+    # scores = run_evaluate_models(x_train, y_train, x_test, y_test)
+
+    ## Stage 4 Run with normed data and compare
+    # scores_from_normed = run_evaluate_models(normed_x_train, y_train, normed_x_test, y_test, print_in_run=0)
+    # scores_from_normed = mark_scores_names_as_normed(scores_from_normed)
+    # check_if_normed_helped_report(scores, scores_from_normed)
+    # best ones are:
+    #     KNeighborsClassifier
+    #     RandomForestClassifier
+
+    ## Stage 5 -Grid search
+    RANDOM_STATE = 40
+    # init knn
+    knn_param_grid = { "n_neighbors":[3, 4], "weights":['uniform', 'distance'], "algorithm":['auto', 'brute']}
+    knn_gs = GridSearchCV(estimator=KNeighborsClassifier(),
+                          scoring='accuracy', n_jobs=-1, param_grid=knn_param_grid)
+    # init rfc
+    rfc_param_grid = {"n_estimators": [300, 500], "max_features": ['auto', 'log2'], "class_weight":['balanced', 'balanced_subsample']}
+    rfc_gs = GridSearchCV(estimator=RandomForestClassifier(random_state=RANDOM_STATE),
+                          scoring='accuracy', n_jobs=-1,param_grid=rfc_param_grid)
+
+    # fit models
+    knn_gs.fit(normed_x_train, y_train)
+
+    rfc_gs.fit(normed_x_train, y_train)
+
+    #
+    # knn_gs.best_estimator_ = "KNeighborsClassifier(n_neighbors=4, weights='distance')"
+    # acc_score = 0.9577777777777777
+    print("K-nearest neighbours algorithm")
+    print(f"best estimator: {knn_gs.best_estimator_}")
+    pred = knn_gs.best_estimator_.predict(normed_x_test)
+    acc_score = accuracy_score(y_test, pred)
+    print(f"accuracy: {round(acc_score,3)}\n")
+
+    # rfc_gs.best_estimator_ = "RandomForestClassifier(class_weight=None, max_features='auto',
+    #                        n_estimators=100, random_state=40)"
+    # acc_score = 0.9577777777777777
+    print("Random forest algorithm")
+    print(f"best estimator: {rfc_gs.best_estimator_}")
+    pred = rfc_gs.best_estimator_.predict(normed_x_test)
+    print(f"accuracy: {accuracy_score(y_test, pred)}")
+
+
+
+
+
+
 
 
