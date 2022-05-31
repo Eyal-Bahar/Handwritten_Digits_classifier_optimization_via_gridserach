64f5bc4e3eb1085cf96e87bc7198800949169a76
Eyal Bahar <80768687+Eyal-Bahar@users.noreply.github.com>
2022-05-31 23:30:36 +0700

    Added some basic documentation

--- Classification of Handwritten Digits/task/main.py
+++ Classification of Handwritten Digits/task/main.py
@@ -9,6 +9,7 @@ Evaluates the models on the test data.
 It normalizes the data (since that gave better results).
 Then, it pefroms a grid search to find the optinal model
 
+The project was based on a project in the Jetbrains Intro to Machine Learning Track.
 """
 
 import tensorflow as tf
@@ -33,7 +34,7 @@ class Loader:
 
     def raw_data_loader(self):
         # Load data for test and train
-        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+        (x_train, y_train), (x_test, y_test) = tf.kesras.datasets.mnist.load_data(path="mnist.npz")
         self.x_train, self.y_train = x_train, y_train
         self.x_test, self.y_test =  x_test, y_test
 
@@ -67,8 +68,9 @@ class Splitter:
     def train_test_hanlder(features, target):
         x_train, x_test, y_train, y_test = train_test_split(features, target, random_state=40, test_size=0.3)
         return x_train, x_test, y_train, y_test
+    
     @staticmethod
-    def stage_two_report(x_train, x_test, y_train, y_test):
+    def shapre_report(x_train, x_test, y_train, y_test):
         print(f"x_train shape:", x_train.shape)
         print(f"x_test shape:", x_test.shape)
         print(f"y_train shape:", y_train.shape)
@@ -199,56 +201,72 @@ def check_if_normed_helped_report(scores, scores_from_normed):
     print_best(scores | scores_from_normed, 2)
 
 
+def normalized_data(x_train, x_test):
+    transformer = Normalizer().fit(x_train)  # fit does nothing.
+    normed_x_train = transformer.transform(x_train)
+    normed_x_test = transformer.transform(x_test)
+    return normed_x_train, normed_x_test
+
+def grid_search_random_forest(RANDOM_STATE):
+    rfc_param_grid = {"n_estimators": [300, 500], "max_features": ['auto', 'log2'], "class_weight":['balanced', 'balanced_subsample']}
+    rfc_gs = GridSearchCV(estimator=RandomForestClassifier(random_state=RANDOM_STATE),
+                        scoring='accuracy', n_jobs=-1,param_grid=rfc_param_grid)
+    return rfc_gs
+
+def grid_search_knc(RANDOM_STATE):
+    knn_param_grid = { "n_neighbors":[3, 4], "weights":['uniform', 'distance'], "algorithm":['auto', 'brute']}
+    knn_gs = GridSearchCV(estimator=KNeighborsClassifier(),
+                          scoring='accuracy', n_jobs=-1, param_grid=knn_param_grid)
+    return knn_gs
+
+
 if __name__ == '__main__':
     ##
     loader = Loader()
     splitter = Splitter()
-    ##from sklearn.datasets import load_iris
-
+    
+    # Set shape for train data
     loader.x_train = loader.set_shape(loader.x_train)
-    x_train, y_train = loader.small_sample()
-    ## stage two
+    
+    # grab only a small sample for easy manipulation
+    x_train, y_train = loader.small_sample(rows=6000) # Comment this line for full sample
+    
+    # split data into train and test. Option - output a report on shape and values
     x_train, x_test, y_train, y_test = Splitter.train_test_hanlder(x_train, y_train)
-    # Splitter.stage_two_report(x_train, x_test, y_train, y_test)
-    ## Norm results
-    transformer = Normalizer().fit(x_train)  # fit does nothing.
-    normed_x_train = transformer.transform(x_train)
-    normed_x_test = transformer.transform(x_test)
-
-    ## Stage 3 Run through models
+    # Splitter.shape_report(x_train, x_test, y_train, y_test)
+    
+    ## Norm data
+    normed_x_train, normed_x_test = normalized_data(x_train, x_test)
+    
+    ## Run through models - for unormalized data
     scores = run_evaluate_models(x_train, y_train, x_test, y_test) # does nothing it was only for checking
-
-    ## Stage 4 Run with normed data and compare
+    ## Run through models - for normed data
     scores_from_normed = run_evaluate_models(normed_x_train, y_train, normed_x_test, y_test, print_in_run=0)
+    
+    ## compare scores
     scores_from_normed = mark_scores_names_as_normed(scores_from_normed)
     check_if_normed_helped_report(scores, scores_from_normed)
-
-    # best ones are:
+    #   Best ones are:
     #     KNeighborsClassifier
     #     RandomForestClassifier
 
-    ## Stage 5 -Grid search
+    ## Initiate a grid search object for k-nearest neighbors and random forest
     RANDOM_STATE = 40
-    # init knn
-    knn_param_grid = { "n_neighbors":[3, 4], "weights":['uniform', 'distance'], "algorithm":['auto', 'brute']}
-    knn_gs = GridSearchCV(estimator=KNeighborsClassifier(),
-                          scoring='accuracy', n_jobs=-1, param_grid=knn_param_grid)
-    # init rfc
-    rfc_param_grid = {"n_estimators": [300, 500], "max_features": ['auto', 'log2'], "class_weight":['balanced', 'balanced_subsample']}
-    rfc_gs = GridSearchCV(estimator=RandomForestClassifier(random_state=RANDOM_STATE),
-                          scoring='accuracy', n_jobs=-1,param_grid=rfc_param_grid)
+    knn_gs = grid_search_knc(RANDOM_STATE)
+    rfc_gs = grid_search_random_forest(RANDOM_STATE)
 
     # fit models
     knn_gs.fit(normed_x_train, y_train)
 
     rfc_gs.fit(normed_x_train, y_train)
 
+    # Show the best parameters for each model
+    
     print("K-nearest neighbours algorithm")
     print(f"best estimator: {knn_gs.best_estimator_}")
     pred = knn_gs.best_estimator_.predict(normed_x_test)
     acc_score = accuracy_score(y_test, pred)
     print(f"accuracy: {round(acc_score,3)}\n")
-
 
     print("Random forest algorithm")
     print(f"best estimator: {rfc_gs.best_estimator_}")
